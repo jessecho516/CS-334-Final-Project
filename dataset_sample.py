11fb3256a9b36214f7da2f89b61fad78a316b1cc
Jesse <chojesse0@gmail.com>
2022-11-28 22:08:57 -0500

    pain

--- dataset_sample.py
+++ dataset_sample.py
@@ -6,36 +6,45 @@ import json
 # converting json to dataframe
 def sample_business(json):
     df = pd.read_json(json, lines=True)
-    df = df.sample(n=40000)
-    print(df.head)
-    df.to_csv('sample_business.csv')
+    restaurant = df.dropna(subset=['categories'])
+    restaurant = restaurant[restaurant['categories'].str.contains("Restaurants")]
+    df.to_csv('sample_restaurant.csv')
+    return restaurant['business_id']
 
-def sample_checkin(json):
+def sample_review(json, business_id):
+    chunks = pd.read_json(json, lines=True, chunksize = 10000)
+    reviews = []
+    for chunk in chunks:
+        df = pd.DataFrame(chunk)
+        review = df.loc[df['business_id'].isin(business_id)]
+        reviews.append(review)
+    reviews = pd.DataFrame(reviews)
+    reviews.to_csv('sample_review.csv')
+    return reviews['user_id']
+    
+def sample_user(json, user_id):
+    chunks = pd.read_json(json, lines=True, chunksize = 10000)
+    users = []
+    for chunk in chunks:
+        df = pd.DataFrame(chunk)
+        user = df.loc[df['user_id'].isin(user_id)]
+        users.append(user)
+    users.to_csv('sample_user.csv')
+    
+def sample_checkin(json, business_id):
     df = pd.read_json(json, lines=True)
-    df = df.sample(n=40000)
-    print(df.head)
-    df.to_csv('sample_checkin.csv')
+    checkin = df.loc[df['business_id'].isin(business_id)]
+    checkin.to_csv('sample_checkin.csv')
 
-def sample_review(json):
-    df = pd.read_json(json, lines=True, nrows = 1000000)
-    df = df.sample(n=40000)
-    print(df.head)
-    df.to_csv('sample_review.csv')
-
-def sample_tip(json):
+def sample_tip(json, business_id):
     df = pd.read_json(json, lines=True)
-    df = df.sample(n=40000)
-    print(df.head)
-    df.to_csv('sample_tip.csv')
+    tip = df.loc[df['business_id'].isin(business_id)]
+    tip.to_csv('sample_tip.csv')
 
-def sample_user(json):
-    df = pd.read_json(json, lines=True, nrows = 1000000)
-    df = df.sample(n=40000)
-    print(df.head)
-    df.to_csv('sample_user.csv')
 
-sample_business("yelp_academic_dataset_business.json")
-sample_checkin("yelp_academic_dataset_checkin.json")
-sample_review("yelp_academic_dataset_review.json")
-sample_tip("yelp_academic_dataset_tip.json")
-sample_user("yelp_academic_dataset_user.json")
+
+business_id = sample_business("yelp_academic_dataset_business.json")
+user_id = sample_review("yelp_academic_dataset_review.json", business_id)
+sample_user("yelp_academic_dataset_user.json", user_id)
+sample_checkin("yelp_academic_dataset_checkin.json", business_id)
+sample_tip("yelp_academic_dataset_tip.json", business_id)
